bb2cb91792ad12c66ff4f20b9dd8916efa8d8eca
Abidin K <abidinkofficial@gmail.com>
2021-10-03 20:35:19 +0300

    Basic game logic implemented

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,36 +2,82 @@ import { useEffect, useState } from "react"
 import styles from "./Home.module.css"
 import Letter from "../../components/Letter"
 import HangedMan from "../../components/HangedMan"
+import logo from "../../assets/hangman-logo-200-w.png"
 
 import words from "../../data/words"
 import { isLetter } from "../../utils"
 
 const Home = () => {
   const [randomWord, setRandomWord] = useState("")
+  const [inputLetter, setInputLetter] = useState(null)
+  const [inputArr, setInputArr] = useState([])
+  const [displayArr, setDisplayArr] = useState([])
+  const [win, setWin] = useState(false)
+  const [wrong, setWrong] = useState(0)
+  const [lose, setLose] = useState(false)
+  console.log(inputArr)
 
   useEffect(() => {
-    document.addEventListener("keydown", handleKeyPress)
+    window.addEventListener("keydown", handleKeyPress)
     setRandomWord(words[Math.floor(Math.random() * 10)])
   }, [])
 
+  useEffect(() => {
+    setDisplayArr(() => [ ...randomWord.split("").map((letter, i) => "_")] )
+  }, [randomWord])
+
+  useEffect(() => {
+    setDisplayArr(() => randomWord.split("").map((letter, i) => {
+      if (inputArr.includes(letter)) {
+        return letter
+      } else {
+        return "_"
+      }
+    }))
+  }, [inputArr])
+
+  useEffect(() => {
+    if (inputLetter && !inputArr.includes(inputLetter)) {
+      setInputArr((arr) => [...arr, inputLetter])
+      if (!randomWord.split("").includes(inputLetter)) {
+        setWrong((w) => w + 1)
+      }
+    }
+    // eslint-disable-next-line
+  }, [inputLetter])
+
+  useEffect(() => {
+    if (displayArr.join("") === randomWord && !!randomWord) {
+      setWin(true)
+    }
+    // eslint-disable-next-line
+  }, [displayArr])
+
+  useEffect(() => {
+    if (wrong >= 5) {setLose(true)}
+  }, [wrong])
+
   const handleKeyPress = (event) => {
-    const input = event.key.toLowerCase()
-    isLetter(input) && console.log(input)
-  } 
+    let input = event.key.toLowerCase()
+    isLetter(input) && setInputLetter(input)
+  }
 
   return (
     <div className={styles["Home"]}>
       <header>
+        <img src={logo} alt="hang react" />
         <h1>birey asmaca</h1>
       </header>
       <div className={styles["game"]}>
         <div className={styles["game-word"]}>
           {
-            randomWord.split("").map((letter, i) => <Letter key={i} letter={letter} />)
+            displayArr.map((letter, i) => <Letter key={i} letter={letter} />)
           }
+          {win && <div>Win</div>}
+          {lose && <div>Game over</div>}
         </div>
         <div className={styles["game-hanged"]}>
-          <HangedMan />
+          <HangedMan wrong={wrong} />
         </div>
       </div>
       <footer>
